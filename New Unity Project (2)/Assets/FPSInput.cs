6c967a73f251137ef82be4fad88fe73196a12d8e
Tosyatosenka <47075068+Tosyatosenka@users.noreply.github.com>
2019-02-16 19:41:21 +0300

    second commit

---
+++ New Unity Project (2)/Assets/FPSInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX, 0, deltaZ);
+        
+    }
+}
