ea08b1a6540f4cd254b44ca645119945d828fe1b
Tosyatosenka <47075068+Tosyatosenka@users.noreply.github.com>
2019-03-27 12:52:05 +0300

    Commit27/03

---
+++ New Unity Project (2D GAME)/Assets/MemoryCard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    public GameObject cardBack;
+    public SceneController controller;
+
+    private int _id;
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
---
+++ New Unity Project (2D GAME)/Assets/SceneController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+   // Start is called before the first frame update
+   void Start()
+    {
+        Vector3 startPos = originalCard.transform.position;
+        for(int i=0; i < gridCols; i++)
+        {
+            for(int j = 0; j<gridRows; j++)
+            {
+                MemoryCard card;
+                if(i == 0 && j==0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+                int id = Random.Range(0, images.Length);
+                originalCard.SetCard(id, images[id]);
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z); 
+
+
+            }
+
+        }
+     }
+
+
+
+        
+    }
+
+   
+
